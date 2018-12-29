b361e50dcb2db45c16554f841e56b3aa2f0522e2
Kevin Hu <contactKevinHu@gmail.com>
2018-12-29 15:31:43 -0500

    got cmake working and program compiles

---
+++ CMakeFiles/LegoTrain.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LegoTrain.dir/LegoTrain.cpp.o"
+  "LegoTrain.pdb"
+  "LegoTrain"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LegoTrain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- LegoTrain.cpp
+++ LegoTrain.cpp
@@ -14,55 +14,55 @@ using namespace cv::face;
 using namespace std;
 
 static void read_csv(const string &filename, vector<Mat> &images, vector<int> &labels, char seperator = ';'){
-		std::ifstream file(filename.c_str(), ifstream:in);
-		if(!file){
-				string error_message = "The filename given seems to be wrong, please doublecheck and try again";
-				CV_Error(-5, error_message); //-5 is StsBadArg
+	std::ifstream file(filename.c_str(), ifstream::in);
+	if(!file){
+		string error_message = "The filename given seems to be wrong, please doublecheck and try again";
+		CV_Error(-5, error_message); //-5 is StsBadArg
+	}
+	string line,                   //The current line in the csv
+	       path,                   //Path to the image
+	       classlabel;             //Label at the end of the image
+	while(getline(file,line)){   
+		stringstream liness(line); 
+		getline(liness, path, seperator);
+		getline(liness, classlabel);
+		if(!path.empty() && !classlabel.empty()) {
+			images.push_back(imread(path, 0));
+			labels.push_back(atoi(classlabel.c_str()));
 		}
-		string line,                   //The current line in the csv
-			   path,                   //Path to the image
-			   classlabel;             //Label at the end of the image
-		while(getline(file,line)){   
-				stringstream liness(line); 
-				getline(liness, path, separator);
-				getline(liness, classlabel);
-				if(!path.empty() && !classlabel.empty()) {
-						images.push_back(imread(path, 0));
-						labels.push_back(atoi(classlabel.c_str()));
-				}
-		}
+	}
 }
 
 int main(int argc, const char *argv[]){
-		//This is to set up all the device constants so I don't have to keep typing them in
-		string fn_haar = "/home/kevin/opencv/data/haarcascades/haarcascade_frontalface_default.xml";
-		string fn_lbp = "/home/kevin/opencv/data/lbpcascades/lbpcascade_frontalface_improved.xml";
-		string fn_csv = "/home/kevin/Desktop/CameramanJohn/faces.txt";
-		int deviceId = 0;
+	//This is to set up all the device constants so I don't have to keep typing them in
+	string fn_haar = "/home/kevin/opencv/data/haarcascades/haarcascade_frontalface_default.xml";
+	string fn_lbp = "/home/kevin/opencv/data/lbpcascades/lbpcascade_frontalface_improved.xml";
+	string fn_csv = "/home/kevin/Desktop/CameramanJohn/faces.txt";
+	int deviceId = 0;
 
-		//Image and label vectors
-		vector<Mat> images;
-		vector<int> labels;
+	//Image and label vectors
+	vector<Mat> images;
+	vector<int> labels;
 
-		// Read in the data (fails if no valid input filename is given, but you'll get an error message):
-		try {
-				read_csv(fn_csv, images, labels);
-		} 
-		catch (cv::Exception& e) {
-				cerr << "Error opening file \"" << fn_csv << "\". Reason: " << e.msg << endl;
-				// nothing more we can do
-				exit(1);
-		}
+	// Read in the data (fails if no valid input filename is given, but you'll get an error message):
+	try {
+		read_csv(fn_csv, images, labels);
+	} 
+	catch (cv::Exception& e) {
+		cerr << "Error opening file \"" << fn_csv << "\". Reason: " << e.msg << endl;
+		// nothing more we can do
+		exit(1);
+	}
 
-		int im_width = images[0].cols;    //Checks how many columns are in the image matrix
-		int im_height = images[0].rows;   //Checks how many rows are in the image matrix
+	int im_width = images[0].cols;    //Checks how many columns are in the image matrix
+	int im_height = images[0].rows;   //Checks how many rows are in the image matrix
 
-		//Creates the LBPH object
-		Ptr<FaceRecognizer> model = LBPHFaceRecognizer::create();
+	//Creates the LBPH object
+	Ptr<FaceRecognizer> model = LBPHFaceRecognizer::create();
 
-		//Training the face classifier
-		cout << "Training..." << endl;
-		model->train(images, labels);
-		cout << "Done training" << endl;		
+	//Training the face classifier
+	cout << "Training..." << endl;
+	model->train(images, labels);
+	cout << "Done training" << endl;		
 
 }
